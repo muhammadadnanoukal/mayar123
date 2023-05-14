12f025b5154b14a9adc20f949309834f9794c715
mayar-ALTANMYA <130453588+mayar-ALTANMYA@users.noreply.github.com>
2023-05-14 17:59:11 +0300

    Add files via upload

---
+++ ALTANMYA_ACCRUAL_TIMEOFF/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'ALTANMYA ACCRUAL TIMEOFF',
+    'version': '1.0',
+    'summary': 'ALTANMYA ACCRUAL TIMEOFF',
+    'description': """ALTANMYA ACCRUAL TIMEOFF """,
+    'category': 'Human Resources/Approvals',
+    'author': 'ALTANMYA - TECHNOLOGY SOLUTIONS',
+    'company': 'ALTANMYA - TECHNOLOGY SOLUTIONS Part of ALTANMYA GROUP',
+    'website': "https://tech.altanmya.net",
+    'depends': ['hr_holidays'],
+    'data': [
+        'views/test_test.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ ALTANMYA_ACCRUAL_TIMEOFF/models/__init__.py
@@ -0,0 +1,1 @@
+from . import hr_accrula_inh
