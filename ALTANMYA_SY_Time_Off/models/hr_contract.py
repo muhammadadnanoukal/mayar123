f5cf78195b3ece2849d89cb9d639ca40df3d5d9e
mayar-ALTANMYA <130453588+mayar-ALTANMYA@users.noreply.github.com>
2023-05-16 17:27:06 +0300

    Add files via upload

---
+++ ALTANMYA_SY_Time_Off/models/hr_contract.py
@@ -0,0 +1,168 @@
+import itertools
+import pytz
+from collections import defaultdict
+
+from odoo import api, fields, models, _
+from odoo.addons.resource.models.resource import datetime_to_string, string_to_datetime, Intervals
+
+
+class HrContract(models.Model):
+    _inherit = 'hr.contract'
+    _description = 'Employee Contract'
+    #
+    # def _get_work_entries_values(self, date_start, date_stop):
+    #     """
+    #     Generate a work_entries list between date_start and date_stop for one contract.
+    #     :return: list of dictionnary.
+    #     """
+    #     contract_vals = self._get_contract_work_entries_values(date_start, date_stop)
+    #
+    #     # {contract_id: ([dates_start], [dates_stop])}
+    #     mapped_contract_dates = defaultdict(lambda: ([], []))
+    #     for x in contract_vals:
+    #         mapped_contract_dates[x['contract_id']][0].append(x['date_start'])
+    #         mapped_contract_dates[x['contract_id']][1].append(x['date_stop'])
+    #
+    #     for contract in self:
+    #         # If we generate work_entries which exceeds date_start or date_stop, we change boundaries on contract
+    #         if contract_vals:
+    #             #Handle empty work entries for certain contracts, could happen on an attendance based contract
+    #             #NOTE: this does not handle date_stop or date_start not being present in vals
+    #             dates_stop = mapped_contract_dates[contract.id][1]
+    #             if dates_stop:
+    #                 date_stop_max = max(dates_stop)
+    #                 if date_stop_max > contract.date_generated_to:
+    #                     contract.date_generated_to = date_stop_max
+    #
+    #             dates_start = mapped_contract_dates[contract.id][0]
+    #             if dates_start:
+    #                 date_start_min = min(dates_start)
+    #                 if date_start_min < contract.date_generated_from:
+    #                     contract.date_generated_from = date_start_min
+    #
+    #     return contract_vals
+    #
+    # def _get_contract_work_entries_values(self, date_start, date_stop):
+    #     start_dt = pytz.utc.localize(date_start) if not date_start.tzinfo else date_start
+    #     end_dt = pytz.utc.localize(date_stop) if not date_stop.tzinfo else date_stop
+    #
+    #     contract_vals = []
+    #     bypassing_work_entry_type_codes = self._get_bypassing_work_entry_type_codes()
+    #
+    #     attendances_by_resource = self._get_attendance_intervals(start_dt, end_dt)
+    #
+    #     resource_calendar_leaves = self.env['resource.calendar.leaves'].search(self._get_leave_domain(start_dt, end_dt))
+    #     # {resource: resource_calendar_leaves}
+    #     leaves_by_resource = defaultdict(lambda: self.env['resource.calendar.leaves'])
+    #     for leave in resource_calendar_leaves:
+    #         leaves_by_resource[leave.resource_id.id] |= leave
+    #
+    #     print('hello', date_start, date_stop)
+    #
+    #     tz_dates = {}
+    #     for contract in self:
+    #         employee = contract.employee_id
+    #         calendar = contract.resource_calendar_id
+    #         resource = employee.resource_id
+    #         tz = pytz.timezone(calendar.tz)
+    #
+    #         attendances = attendances_by_resource[resource.id]
+    #
+    #         # Other calendars: In case the employee has declared time off in another calendar
+    #         # Example: Take a time off, then a credit time.
+    #         # YTI TODO: This mimics the behavior of _leave_intervals_batch, while waiting to be cleaned
+    #         # in master.
+    #         resources_list = [self.env['resource.resource'], resource]
+    #         result = defaultdict(lambda: [])
+    #         for leave in itertools.chain(leaves_by_resource[False], leaves_by_resource[resource.id]):
+    #             for resource in resources_list:
+    #                 # Global time off is not for this calendar, can happen with multiple calendars in self
+    #                 if resource and leave.calendar_id and leave.calendar_id != calendar and not leave.resource_id:
+    #                     continue
+    #                 tz = tz if tz else pytz.timezone((resource or contract).tz)
+    #                 if (tz, start_dt) in tz_dates:
+    #                     start = tz_dates[(tz, start_dt)]
+    #                 else:
+    #                     start = start_dt.astimezone(tz)
+    #                     tz_dates[(tz, start_dt)] = start
+    #                 if (tz, end_dt) in tz_dates:
+    #                     end = tz_dates[(tz, end_dt)]
+    #                 else:
+    #                     end = end_dt.astimezone(tz)
+    #                     tz_dates[(tz, end_dt)] = end
+    #                 dt0 = string_to_datetime(leave.date_from).astimezone(tz)
+    #                 dt1 = string_to_datetime(leave.date_to).astimezone(tz)
+    #                 result[resource.id].append((max(start, dt0), min(end, dt1), leave))
+    #         mapped_leaves = {r.id: Intervals(result[r.id]) for r in resources_list}
+    #         leaves = mapped_leaves[resource.id]
+    #
+    #         real_attendances = attendances - leaves
+    #         if contract.has_static_work_entries() or not leaves:
+    #             # Empty leaves means empty real_leaves
+    #             real_leaves = attendances - real_attendances
+    #         else:
+    #             # In the case of attendance based contracts use regular attendances to generate leave intervals
+    #             static_attendances = calendar._attendance_intervals_batch(
+    #                 start_dt, end_dt, resources=resource, tz=tz)[resource.id]
+    #             real_leaves = static_attendances & leaves
+    #
+    #         if not contract.has_static_work_entries():
+    #             # An attendance based contract might have an invalid planning, by definition it may not happen with
+    #             # static work entries.
+    #             # Creating overlapping slots for example might lead to a single work entry.
+    #             # In that case we still create both work entries to indicate a problem (conflicting W E).
+    #             split_attendances = []
+    #             for attendance in real_attendances:
+    #                 if attendance[2] and len(attendance[2]) > 1:
+    #                     split_attendances += [(attendance[0], attendance[1], a) for a in attendance[2]]
+    #                 else:
+    #                     split_attendances += [attendance]
+    #             real_attendances = split_attendances
+    #
+    #         # A leave period can be linked to several resource.calendar.leave
+    #         split_leaves = []
+    #         for leave_interval in leaves:
+    #             if leave_interval[2] and len(leave_interval[2]) > 1:
+    #                 split_leaves += [(leave_interval[0], leave_interval[1], l) for l in leave_interval[2]]
+    #             else:
+    #                 split_leaves += [(leave_interval[0], leave_interval[1], leave_interval[2])]
+    #         leaves = split_leaves
+    #
+    #         # Attendances
+    #         default_work_entry_type = contract._get_default_work_entry_type()
+    #         for interval in real_attendances:
+    #             work_entry_type = 'work_entry_type_id' in interval[2] and interval[2].work_entry_type_id[:1]\
+    #                 or default_work_entry_type
+    #             # All benefits generated here are using datetimes converted from the employee's timezone
+    #             contract_vals += [dict([
+    #                 ('name', "%s: %s" % (work_entry_type.name, employee.name)),
+    #                 ('date_start', interval[0].astimezone(pytz.utc).replace(tzinfo=None)),
+    #                 ('date_stop', interval[1].astimezone(pytz.utc).replace(tzinfo=None)),
+    #                 ('work_entry_type_id', work_entry_type.id),
+    #                 ('employee_id', employee.id),
+    #                 ('contract_id', contract.id),
+    #                 ('company_id', contract.company_id.id),
+    #                 ('state', 'draft'),
+    #             ] + contract._get_more_vals_attendance_interval(interval))]
+    #
+    #         for interval in real_leaves:
+    #             # Could happen when a leave is configured on the interface on a day for which the
+    #             # employee is not supposed to work, i.e. no attendance_ids on the calendar.
+    #             # In that case, do try to generate an empty work entry, as this would raise a
+    #             # sql constraint error
+    #             if interval[0] == interval[1]:  # if start == stop
+    #                 continue
+    #             leave_entry_type = contract._get_interval_leave_work_entry_type(interval, leaves, bypassing_work_entry_type_codes)
+    #             interval_start = interval[0].astimezone(pytz.utc).replace(tzinfo=None)
+    #             interval_stop = interval[1].astimezone(pytz.utc).replace(tzinfo=None)
+    #             contract_vals += [dict([
+    #                 ('name', "%s%s" % (leave_entry_type.name + ": " if leave_entry_type else "", employee.name)),
+    #                 ('date_start', interval_start),
+    #                 ('date_stop', interval_stop),
+    #                 ('work_entry_type_id', leave_entry_type.id),
+    #                 ('employee_id', employee.id),
+    #                 ('company_id', contract.company_id.id),
+    #                 ('state', 'draft'),
+    #                 ('contract_id', contract.id),
+    #             ] + contract._get_more_vals_leave_interval(interval, leaves))]
+    #     return contract_vals
---
+++ ALTANMYA_SY_Time_Off/models/hr_work_entry.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+from contextlib import contextmanager
+from dateutil.relativedelta import relativedelta
+import itertools
+from psycopg2 import OperationalError
+
+from odoo import api, fields, models, tools, _
+
+from odoo.exceptions import ValidationError
+
+
+class HrWorkEntry(models.Model):
+    _inherit = 'hr.work.entry'
+
+    is_holiday_entry = fields.Boolean('Is holiday entry?', default=False)
+
+    def write(self, vals):
+        skip_check = not bool({'date_start', 'date_stop', 'employee_id', 'work_entry_type_id', 'active'} & vals.keys())
+        if 'state' in vals:
+            if vals['state'] == 'draft':
+                vals['active'] = True
+            elif vals['state'] == 'cancelled':
+                vals['active'] = False
+                skip_check &= all(self.mapped(lambda w: w.state != 'conflict'))
+
+        if 'active' in vals:
+            vals['state'] = 'draft' if vals['active'] else 'cancelled'
+
+        with self._error_checking(skip=skip_check):
+            return super(models.Model, self).write(vals)
+
+    @contextmanager
+    def _error_checking(self, start=None, stop=None, skip=False, employee_ids=False):
+        """
+        Context manager used for conflicts checking.
+        When exiting the context manager, conflicts are checked
+        for all work entries within a date range. By default, the start and end dates are
+        computed according to `self` (min and max respectively) but it can be overwritten by providing
+        other values as parameter.
+        :param start: datetime to overwrite the default behaviour
+        :param stop: datetime to overwrite the default behaviour
+        :param skip: If True, no error checking is done
+        """
+        try:
+            print('ULTRA ', self.env.context.get('hr_work_entry_no_check', False))
+            skip = skip or self.env.context.get('hr_work_entry_no_check', False)
+            start = start or min(self.mapped('date_start'), default=False)
+            stop = stop or max(self.mapped('date_stop'), default=False)
+            if not skip and start and stop:
+                work_entries = self.sudo().with_context(hr_work_entry_no_check=True).search([
+                    ('date_start', '<', stop),
+                    ('date_stop', '>', start),
+                    ('state', 'not in', ('validated', 'cancelled')),
+                    ('is_holiday_entry', '!=', True)
+                ])
+                print('reseting ', work_entries)
+                work_entries._reset_conflicting_state()
+            yield
+        except OperationalError:
+            # the cursor is dead, do not attempt to use it or we will shadow the root exception
+            # with a "psycopg2.InternalError: current transaction is aborted, ..."
+            skip = True
+            raise
+        finally:
+            if not skip and start and stop:
+                # New work entries are handled in the create method,
+                # no need to reload work entries.
+                work_entries.exists()._check_if_error()
